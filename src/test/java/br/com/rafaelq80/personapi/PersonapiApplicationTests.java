83b51f3b7a85ce154ca0015169daf1ab72613bba
Rafael Queiroz <rafaelproinfo@gmail.com>
2021-04-24 01:33:45 -0300

    Primeiro Commit - Teste Heroku

---
+++ src/test/java/br/com/rafaelq80/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafaelq80.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
